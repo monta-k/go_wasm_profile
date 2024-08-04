865d6aa81822ff19242871568e27b20ebedf83ee
Kobayashi Shunta <monta.k0523@gmail.com>
2024-08-04 19:09:10 +0900

    chore: Update Terraform commands to use `-chdir` option for directory path

---
+++ cloudflare/variables.tf
@@ -0,0 +1,9 @@
+variable "cloudflare_email" {
+  type        = string
+  description = "The email associated with the Cloudflare account"
+}
+
+variable "cloudflare_api_token" {
+  type        = string
+  description = "The API token for the Cloudflare account"
+}
