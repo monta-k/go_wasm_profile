8bc02964691b420258c53de730cf83708d6322e9
Kobayashi Shunta <monta.k0523@gmail.com>
2024-08-05 00:00:10 +0900

    chore: Update Cloudflare worker script to use ES modules syntax

--- cloudflare/worker.js
+++ cloudflare/worker.js
@@ -1,7 +1,15 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
 
-async function handleRequest(request) {
-  return new Response("Hello, world!");
-}
+export default {
+  async fetch(request, env, ctx) {
+    return new Response("Hello World!");
+  },
+};
