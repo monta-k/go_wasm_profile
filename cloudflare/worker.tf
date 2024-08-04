5d5f08fea566d88f1d6fdde3fd4ad8221f006a2f
Kobayashi Shunta <monta.k0523@gmail.com>
2024-08-05 00:21:28 +0900

    chore: Update Cloudflare worker route pattern for mnt_profile.monta-k0523.workers.dev

--- cloudflare/worker.tf
+++ cloudflare/worker.tf
@@ -4,8 +4,3 @@ resource "cloudflare_worker_script" "mnt_profile" {
   content = file("${path.module}/worker.js")
   module     = true
 }
-
-resource "cloudflare_worker_route" "mnt_profile_route" {
-  pattern = "monta-k0523.workers.dev/*"
-  script_name = cloudflare_worker_script.mnt_profile.name
-}
