eb9d81dff2a841c96086d27af2a32665ec132742
Kobayashi Shunta <monta.k0523@gmail.com>
2024-08-04 23:58:18 +0900

    chore: Add module flag to Cloudflare worker script resource

--- cloudflare/worker.tf
+++ cloudflare/worker.tf
@@ -2,4 +2,5 @@ resource "cloudflare_worker_script" "mnt_profile" {
   account_id = var.cloudflare_account_id
   name = "mnt_profile"
   content = file("${path.module}/worker.js")
+  module     = true
 }
