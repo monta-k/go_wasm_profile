39cd11d1f161fdbbbacc712e20e8987c3d7fdb18
Kobayashi Shunta <monta.k0523@gmail.com>
2024-08-04 23:57:02 +0900

    chore: Update Cloudflare Terraform configuration to include organization and workspace settings

--- cloudflare/provider.tf
+++ cloudflare/provider.tf
@@ -1,6 +1,15 @@
 terraform {
   required_version = ">= 1.9"
 
+  cloud {
+    organization = "mnt_profile" 
+    hostname = "app.terraform.io"
+
+    workspaces { 
+      name = "mnt_profile" 
+    } 
+  }
+
   required_providers {
     cloudflare = {
       source = "cloudflare/cloudflare"
