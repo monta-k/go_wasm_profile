75233b2011cc4eb99fe44741060d07cb2a120c24
Kobayashi Shunta <monta.k0523@gmail.com>
2024-08-04 19:21:28 +0900

    chore: Update Cloudflare Terraform configuration to include account ID

--- cloudflare/variables.tf
+++ cloudflare/variables.tf
@@ -1,9 +1,9 @@
-variable "cloudflare_email" {
-  type        = string
-  description = "The email associated with the Cloudflare account"
-}
-
 variable "cloudflare_api_token" {
   type        = string
   description = "The API token for the Cloudflare account"
+}
+
+variable "cloudflare_account_id" {
+  type        = string
+  description = "The account ID for the Cloudflare account"
 }
--- cloudflare/worker.tf
+++ cloudflare/worker.tf
@@ -1,4 +1,5 @@
 resource "cloudflare_worker_script" "mnt_profile" {
+  account_id = var.cloudflare_account_id
   name = "mnt_profile"
   
   content = file("${path.module}/worker.js")
