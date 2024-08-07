f55ecd2950425db216aff4b9bb8f060290dbd940
Kobayashi Shunta <monta.k0523@gmail.com>
2024-08-08 01:21:42 +0900

    chore: Update Cloudflare worker script for mnt_profile

--- terraform/worker.tf
+++ terraform/worker.tf
@@ -1,6 +1,28 @@
 resource "cloudflare_worker_script" "mnt_profile" {
   account_id = var.cloudflare_account_id
   name = "mnt_profile"
-  content = file("${path.module}/worker.js")
-  module     = true
+  content = <<EOF
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+  async fetch(request, env, ctx) {
+    return new Response("Hello World!");
+  },
+};
+EOF
+  module = true
+
+  lifecycle {
+    ignore_changes = [
+      content
+    ]
+  }
 }
