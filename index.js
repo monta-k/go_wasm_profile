f0e8f361a9dbfc8a20797653531350ac3ca4699c
Kobayashi Shunta <monta.k0523@gmail.com>
2024-08-11 13:17:10 +0900

    feat: add footer

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import "./wasm_exec.js";
 import module from "./build/app.wasm";
-import logo from "./static/images/logo.png";
 
 export default {
   async fetch(request, env, ctx) {
     const go = new Go();
     const result = await WebAssembly.instantiate(module, go.importObject);
     go.run(result);
-    const response = serveHTML(logo);
+    const response = serveHTML();
     return new Response(response, {
       headers: {
         "content-type": "text/html;charset=UTF-8",
